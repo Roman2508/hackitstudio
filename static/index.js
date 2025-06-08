d814e64d2e4a938849a0398fcb5bc641740dcc22
Roman Ptashnyk <100701048+Roman2508@users.noreply.github.com>
2025-06-08 10:01:37 +0300

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,14 @@
+const countries = [
+  { title: "Іспанія", flag: "Flag_of_Spain.svg", background: "spain.webp" },
+  { title: "Португалія", flag: "", background: "" },
+  { title: "Греція", flag: "", background: "" },
+  { title: "Ізраїль", flag: "", background: "" },
+  { title: "Туреччина", flag: "", background: "" },
+  { title: "Єгипет", flag: "", background: "" },
+  { title: "Туніс", flag: "", background: "" },
+  { title: "Таїланд", flag: "", background: "" },
+  { title: "ОАЕ", flag: "", background: "" },
+  { title: "Шрі Ланка", flag: "", background: "" },
+  { title: "Індія", flag: "", background: "" },
+  { title: "Домініканська р.", flag: "", background: "" },
+]
